72136cc6f01c43232432d3cbc8bc40ff3636cf80
melitina915 <90691824+melitina915@users.noreply.github.com>
2022-02-23 03:34:51 +0900

    Update activitycreate_process.php

--- webproject/activity/activitycreate_process.php
+++ webproject/activity/activitycreate_process.php
@@ -1,4 +1,4 @@
 <?php
 file_put_contents('webproject/activity/'.$_POST['title'], $_POST['description']);
-header('Location: /webproject/activity/activity.php?id='.$_POST['title']);
+header('Location: /webproject/activity/activity.html?id='.$_POST['title']);
 ?>
